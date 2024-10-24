ac53b4b5334ed28105be6703d68f352dafdee0aa
Insaram Alam Siddique <alaminsaram92@gmail.com>
2024-10-25 03:24:25 +0500

    Initial Commit

---
+++ FlashUserApp/FlashUserAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashUserAppApp.swift
+//  FlashUserApp
+//
+//  Created by Insaram Alam Siddique on 25/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct FlashUserAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
